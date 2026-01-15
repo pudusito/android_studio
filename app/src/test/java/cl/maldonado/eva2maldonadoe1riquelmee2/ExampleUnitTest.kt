67cd11f6eb14da06ed0b9da25dd90358833fa351
Benjamin (Pudu) Maldonado B. <benjaminmaldonadobarrales@gmail.com>
2026-01-15 15:49:43 -0300

    upp

---
+++ app/src/test/java/cl/maldonado/eva2maldonadoe1riquelmee2/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package cl.maldonado.eva2maldonadoe1riquelmee2
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
